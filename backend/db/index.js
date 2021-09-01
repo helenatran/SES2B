3182172057692d7c6ee2db840835c426d48588e9
Adalita <adalitayoung@gmail.com>
2021-09-01 14:15:11 +1000

    ATL-18 ATL-19 WIP implementing uploading of recording, downloading of recording, and deleting of recording.

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,8 +1,10 @@
 var url = "mongmongodb+srv://Rheshav:SBgxypqdhUv859Q@sesg3.8gnmg.azure.mongodb.net/<dbname>?retryWrites=true&w=majority"; //Connection String
 const MongoClient = require('mongodb').MongoClient;
+const mongodb = require("mongodb");
 const dbName = 'SES2B'; //MongoDB specified Library Cluster
 
 var _db;
+var _bucket;
 
 module.exports = {
     connectToServer: function( callback ) {
@@ -11,8 +13,22 @@ module.exports = {
             return callback( err );
         });
     },
+
+    createGridBucket: function(callback) {
+        try {
+            _bucket = new mongodb.GridFSBucket(_db, { bucketName: 'videoStorageBucket'});
+            return(callback(null))
+        }
+        catch (err){
+            return callback(err);
+        }
+    },
     
     getDb: function() {
         return _db;
+    },
+
+    getBucket: function() {
+        return _bucket;
     }
 }
