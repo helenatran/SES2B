3ee69f29915f63e0bd4add9fafdcc322929f9fe4
Gabrielle Walker <13196732@student.uts.edu.au>
2021-09-09 09:01:20 +1000

    User CRUD operations and api completed, may need small tweaks in the future

--- react-backend/controllers/user-ctrl.js
+++ react-backend/controllers/user-ctrl.js
@@ -1,17 +1,81 @@
-const dbUtil = require("../db")
-var db = dbUtil.getDb();
+const User = require("../models/user-model");
 
+//create new user -> really only for developer use at the moment
+createUser = (req, res) => {
+	const newUser = new User(req.body);
+
+	User.find({ email: newUser.email }).exec(function (err, users) {
+		if (users.length) {
+			return res.status(400).json({
+				success: false,
+				error: 'User email already in use',
+			});
+		}
+		else {
+			newUser.save((err, result) => {
+				if (err) {
+				  res.status(500).json(err);
+				} else {
+					res.status(201).json({
+						success: true,
+						email: newUser.email,
+						message: 'User Added',
+					});    
+				}
+			});
+		}
+	});
+}
+
+//Get all users
 getUsers = (req, res) => {
-    db.collection('users').find().toArray(function(err, results){
+    User.find((err, results) => {
 		if (err) {
 			res.status(500).json(err)
 		}
 		else{
 			res.json(results)
 		}
-	})
+	});
+}
+
+//Get specific user by email
+getUserByEmail = (req, res) => {
+	User.findOne({ email: req.params.email }, (err, result) => {
+		if (err) {
+		  res.status(500).json(err);
+		} else {
+		  res.json(result);
+		}
+	});	
+}
+
+//update user preferred name and/or mobile number
+updateUser = (req, res) => {
+	User.updateOne(
+		{ email: req.params.email },
+		{ $set: 
+			{
+				preferred_name: req.params.preferred_name, 
+				mobile: req.params.mobile 
+			}
+		},
+		(err, result) => {
+		  if (err) {
+			res.status(500).json(err);
+		  } else {
+			return res.status(200).json({
+				success: true,
+				message: "User updated",
+			});
+		  }
+		}
+	);
 }
 
 module.exports = {
-    getUsers
+	createUser,
+    getUsers,
+	getUserByEmail,
+	updateUser
 }
--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 const UserCtrl = require('../controllers/user-ctrl')
 
+router.post('/create-user', UserCtrl.createUser);
 router.get('/get-users', UserCtrl.getUsers);
+router.get('/get-user/:email', UserCtrl.getUserByEmail);
+router.post('/update-user/:email/:preferred_name/:mobile', UserCtrl.updateUser);
 
 module.exports = router;
