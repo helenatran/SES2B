c4c780255975d2b79b77a0c3822c094b4e97a9f8
Adalita <adalitayoung@gmail.com>
2021-09-15 12:53:46 +1000

    Created method and api endpoint to log misconduct for the time the method is called.

--- backend/routes/exam_allocation.js
+++ backend/routes/exam_allocation.js
@@ -7,5 +7,6 @@ router.get("/get-exam-allocations", ExamAllocationCtrl.getAllExamAllocations);
 router.get("/get-exam-allocation/:user_id/:exam_id", ExamAllocationCtrl.getExamAllocation);
 router.get("/update-exam-allocation/:user_id/:exam_id", ExamAllocationCtrl.updateExamAllocation);
 router.get("/delete-exam-allocation/:user_id/:exam_id", ExamAllocationCtrl.deleteExamAllocation);
+router.get("/log-misconduct/:user_id/:exam_id", ExamAllocationCtrl.logMisconduct)
 
 module.exports = router;
