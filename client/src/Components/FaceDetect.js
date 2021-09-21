4e4dec7e516058ec3bf239446a0994b7125bafc1
Nickg1995 <nickg1950@hotmail.com>
2021-09-22 00:23:31 +1000

    faceDetect

--- client/src/Components/FaceDetect.js
+++ client/src/Components/FaceDetect.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import * as faceapi from "face-api.js";
 
-const imageUpload = document.getElementById('imageUpload')
-const MODEL_URL = './models'
-const container = document.createElement('div') //1
+import Webcam from "react-webcam";
+const WebcamComponent = () => <Webcam />;
+
+const videoConstraints = {
+    width: 220,
+    height: 200,
+    facingMode: "user"
+};
+
+const MODEL_URL = './models';
+const container = document.createElement('div');
 
 let img
 let canvas
@@ -20,8 +28,8 @@ const mystyle = {
     zIndex: -1
 };
 
-
 Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
     faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
     faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL), //detect where characters face is
     faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL)
@@ -32,78 +40,100 @@ class FaceDetect extends Component {
         super(props);
         this.state = {
             image: null,
-            canvas: null
-        };
+            canvas: null,
+            screenshot: null
 
+        };
         this.imageUpload = this.imageUpload.bind(this);
     }
 
+    setRef = webcam => {
+        this.webcam = webcam;
+    };
+
+    capture = () => {
+        this.setState({screenshot: this.webcam.getScreenshot()});
+    };
+
 
     imageUpload = async event => {
-        if(img) img.remove()
-        if(canvas) canvas.remove()
-        const labeledFaceDescriptors = await loadLableImages()
-        const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-        console.log(typeof document.getElementById('root'))
+        if(img) img.remove();
+        if(canvas) canvas.remove();
 
-        img = await faceapi.bufferToImage(event.target.files[0])
-        img.style.position = 'absolute'
-        container.append(img)
+        const labeledFaceDescriptors = await loadLableImages();
+        const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
 
-        canvas = faceapi.createCanvasFromMedia(img)
-        canvas.style.position = 'absolute'
-        container.append(canvas)
+        let b64 = await fetch(this.state.screenshot);
+        let blob = await b64.blob();
+
+        img = await faceapi.bufferToImage(blob);
+        img.style.position = 'absolute';
+        container.append(img);
+
+        canvas = faceapi.createCanvasFromMedia(img);
+        canvas.style.position = 'absolute';
+        container.append(canvas);
 
         const displaySize = {
             width: img.width,
             height: img.height
-        }
+        };
 
-        faceapi.matchDimensions(canvas, displaySize)
-        this.setState({image: img})
-        const detections = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceDescriptors()
-        const resizeDetections = faceapi.resizeResults(detections, displaySize)
-        const results = resizeDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+        faceapi.matchDimensions(canvas, displaySize);
+        this.setState({image: img});
+        const detections = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceDescriptors();
+        document.body.append(detections.length);
+        const resizeDetections = faceapi.resizeResults(detections, displaySize);
+        const results = resizeDetections.map(d => faceMatcher.findBestMatch(d.descriptor));
         results.forEach((result, i) => {
-            const box = resizeDetections[i].detection.box
-            const drawBox = new faceapi.draw.DrawBox(box, {label: result.toString()})
+            const box = resizeDetections[i].detection.box;
+            const drawBox = new faceapi.draw.DrawBox(box, {label: result.toString()});
             drawBox.draw(canvas)
-        })
-
+        });
         document.body.append(container)
-
     };
 
     render(){
         return(
-        <div style={mystyle}>
-            <script src="face-api.min.js" defer></script>
-            Face Detection
-            <body>
-                <input
-                    accept="image/*"
-                    className="input"
-                    id="icon-button-file"
-                    type="file"
-                    onChange={this.imageUpload}
+        <div className={mystyle}>
+            <div className="webcam-container">
+                <Webcam
+                    audio={false}
+                    height={200}
+                    ref={this.setRef}
+                    screenshotFormat="image/jpeg"
+                    width={220}
+                    videoConstraints={videoConstraints}
                 />
-            </body>
+                <button onClick={(e) => {
+                    e.preventDefault();
+                    this.capture()
+                    this.imageUpload()}
+                }>capture screenshot</button>
+            </div>
+            {/*<body>*/}
+            {/*    <input*/}
+            {/*        accept="image/*"*/}
+            {/*        className="input"*/}
+            {/*        id="icon-button-file"*/}
+            {/*        type="file"*/}
+            {/*        onChange={this.imageUpload}*/}
+            {/*    />Chooose File*/}
+            {/*</body>*/}
         </div>
-
         )
     }
 }
 
-
 async function start() {
-    document.body.append('Loaded')
+    document.body.append('Loaded');
 }
 
 function loadLableImages(){
-    const labels = ['Black Widow'];
+    const labels = ['NickTest'];
     const descriptions = [];
     return Promise.all(labels.map(async label => {
-       for(let i = 1; i <= 5; i++){
+       for(let i = 1; i <= 1; i++){
            const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/Nickg1995/image-upload/main/labeled_images/${label}/${i}.jpg`)
            const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
            descriptions.push(detections.descriptor)
