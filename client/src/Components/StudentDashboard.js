dd8c7072c41a5775266c7ba404cebd40aa380686
TheerthaS <62228716+TheerthaS@users.noreply.github.com>
2021-09-27 20:46:47 +1000

    Create component

---
+++ client/src/Components/StudentDashboard.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./StudentDashboard.css";
+
+
+
+
+
+
+class StudentDashboard extends Component {
+
+	render() {
+		return (
+			<div>
+				
+				<div className="App-header">
+					this is student StudentDashboard
+				</div>
+			</div>
+		)
+	}
+}
+
+export default StudentDashboard;
