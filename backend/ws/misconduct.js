72d05aa76a99d00ef848321ff88c4db7c22b9231
adalitayoung <adalitayoung@gmail.com>
2021-09-30 10:05:17 +1000

    Update misconduct.js

--- backend/ws/misconduct.js
+++ backend/ws/misconduct.js
@@ -1,25 +1,16 @@
 const ExamAllocation = require("../models/ExamAllocation");
 
 module.exports = (io) => {
-
-    var userId = "initialUserId";
-
+    ExamAllocation.watch([], {
+        fullDocument: 'updateLookup'
+    }).on('change', (change) => {
+          const userId = change.fullDocument.user_id;
+          io.to(userId).emit("misconduct", change.fullDocument.misconduct_detection_times.length);
+    });
     io.on("connection", (socket) => {
-        socket.on("start-listening", (id) => {
-            userId = id;
-            console.log("Listening to "+ userId)
+        socket.on("start-listening", (userId) => {
+            console.log("Listening to " + userId)
             socket.join(userId) //start listening to this id
         });
-
-        ExamAllocation.watch([], {
-            fullDocument: 'updateLookup'
-        }).on('change', (change) => {
-            // If the updated document matches the user watching
-            if (change.fullDocument.user_id === userId){
-                io.to(userId).emit("misconduct",change.fullDocument.misconduct_detection_times.length);
-            }
-        })
     })
-    
-        
 }
