3345394dd7b3fcab1318510c18f712b456d72ae0
sukhpreetp <sukhpreet123@outlook.com>
2021-10-01 20:10:35 +1000

    Attempt to connect db

--- backend/ws/index.js
+++ backend/ws/index.js
@@ -1,7 +1,6 @@
 function startWebSocketServer(io) {
   // Initialise routes
   require('./video')(io.of('/video'));
-  require('./misconduct')(io.of('/misconduct'))
 }
 
 module.exports = startWebSocketServer;
