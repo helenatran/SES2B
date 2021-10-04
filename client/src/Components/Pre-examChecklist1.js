0e823b3877de58f4fafd7fe25d0de0e0cef9bd4f
TheerthaS <62228716+TheerthaS@users.noreply.github.com>
2021-10-05 00:36:53 +1100

    Basic UI

--- client/src/Components/Pre-examChecklist1.js
+++ client/src/Components/Pre-examChecklist1.js
@@ -1,21 +1,66 @@
 import React, { Component } from "react";
 import "./Pre-examChecklist1.css";
-
-
-
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+ 
+  Image,
+} from "react-bootstrap";
+import PlaceHolderImage from "../Assets/profile.png";
 
 class PreExamChecklist1 extends Component {
-	// Placeholder code for Account page
-	// Used during setup of react-router
-	render() {
-		return (
-			<div>
-				<Navbar />
-					This is the CHECKLIST page
-				</div>
-			
-		)
-	}
+  render(
+  
+  ) {
+    return (
+      <div className="App ">
+        <Container fluid>
+          <Row>
+            <Col>
+              <div className="column1-contents ">
+                <h1>Pre-Exam Checklist</h1>
+                <ul class="blackCheck">
+                  <li>
+                    <p className="li-bold">Examinee Identification</p>
+                  </li>
+                  <li>
+                    <p className="li-bold">Object detection</p>
+                  </li>
+                  <li>
+                    <p className="li-bold">Start exam</p>
+                  </li>
+                </ul>
+                <p>
+                  Adjust your camera so full face can be seen then click
+                  continue.
+                </p>
+              </div>
+            </Col>
+
+            <Col className="  col2 ">
+              <div class="column2-contents ">
+                <Image className="PlaceHolderImage" src={PlaceHolderImage} />
+
+                <div class="padding-top-button">
+                  <Button
+                    style={{ height: "60px" }}
+                    className="  button-startExam"
+                  >
+                    Continue
+                  </Button>
+                  <div class="go-back-link">
+                    <a>Go back</a>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default PreExamChecklist1;
